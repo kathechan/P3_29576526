bf8aaa4f9cce9fe8e6ba140296093cdec8ac56f1
Katherine Perez <katherineperez125@gmail.com>
2023-11-04 02:00:35 -0400

    task 1 V1.2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+const request = require('request');
+var md5 = require ('md5');
+const sqlite3 = require('sqlite3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/LoginYRegistro.ejs', function (req, res, next) {
+  res.render('LoginYRegistro.ejs');
 });
 
 module.exports = router;
