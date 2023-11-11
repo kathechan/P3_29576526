f542268620484dc2835fa54c1aa7d137de706d68
Katherine Perez <katherineperez125@gmail.com>
2023-11-10 20:47:37 -0400

    task 1 v1.7

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
-const sqlite3 = require('sqlite3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
